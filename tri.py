a6c88c9b0b7ee2b9df92896ec32e96e9da384f38
sebas <78992252+SebasMena28@users.noreply.github.com>
2022-04-10 20:38:33 -0500

    control supremo
    espero que funque

--- tri.py
+++ tri.py
@@ -33,12 +33,12 @@ def esnumero():
        try:
            entrada = float(entrada)
            while(entrada <= 0):
-                print('El valor de un lado debe ser superior a 0')
+                print('El valor debe ser superior a 0')
                 entrada = input('Ingrese de nuevo: ')
                 entrada = float(entrada)
            return entrada
        except ValueError:
-           print ("CHUPEEEMOOOOOOOOOOS!!!!!!!!!! y vuelve a ingresar otro valor, que sea numero, gil")
+           print ("Error! Los textos no pueden ser ingresados. Intente de nuevo: ")
 
 
 # función para contar los lados iguales del triangulo
@@ -86,17 +86,11 @@ def main():
         print('Ingrese el valor del lado 1: ')
         lado1 = esnumero()
 
-        # ingreso del valor del lado 2 y control de este
-        lado2 = float(input('Ingrese el valor del lado 2: '))
-        while(lado2 <= 0):
-            print('El valor de un lado debe ser superior a 0')
-            lado2 = float(input('Ingrese el valor del lado 2: '))
+        print('Ingrese el valor del lado 2: ')
+        lado2 = esnumero()
 
-        # ingreso del valor del lado 3 y control de este
-        lado3 = float(input('Ingrese el valor del lado 3: '))
-        while(lado3 <= 0):
-            print('El valor de un lado debe ser superior a 0')
-            lado3 = float(input('Ingrese el valor del lado 3: '))
+        print('Ingrese el valor del lado 3: ')
+        lado3 = esnumero()
 
         print()
 
@@ -120,17 +114,12 @@ def main():
             print('Gracias por usar el sistema :)')
     
     elif(resp == '2'):
-        # ingreso del valor de la base y control de este
-        base = float(input('Ingrese el valor de la base: '))
-        while(base <= 0):
-            print('El valor de la base debe ser superior a 0')
-            base = float(input('Ingrese el valor de la base: '))
 
-        # ingreso del valor de la altura y control de este
-        altura = float(input('Ingrese el valor de la altura: '))
-        while(altura <= 0):
-            print('El valor de la altura debe ser superior a 0')
-            altura = float(input('Ingrese el valor de la altura: '))
+        print('Ingrese el valor de la base: ')
+        base = esnumero()
+
+        print('Ingrese el valor de la altura: ')
+        altura = esnumero()
 
         print("El triangulo se identifica como uno de tipo TRIÁNGULO RECTÁNGULO")
         print('El área es: ', round(calcular2(base,altura),3) )
@@ -152,23 +141,15 @@ def main():
             print('Gracias por usar el sistema ')
     
     elif(resp == '3'):
-        # ingreso del valor del lado 1 y control de este
-        lado1 = float(input('Ingrese el valor del lado 1: '))
-        while(lado1 <= 0):
-            print('El valor de un lado debe ser superior a 0')
-            lado1 = float(input('Ingrese el valor del lado 1: '))
 
-        # ingreso del valor del lado 2 y control de este
-        lado2 = float(input('Ingrese el valor del lado 2: '))
-        while(lado2 <= 0):
-            print('El valor de un lado debe ser superior a 0')
-            lado2 = float(input('Ingrese el valor del lado 2: '))
-        
-        ang1 = float(input('Ingrese el valor del ángulo: '))
-        while(ang1<= 0):
-            print('Un ángulo no puede tomar el valor de ', ang1)
-            print('Intente de nuevo...')
-            ang1 = float(input('Ingrese el valor del ángulo: '))
+        print('Ingrese el valor del lado 1: ')
+        lado1 = esnumero()
+
+        print('Ingrese el valor del lado 2: ')
+        lado2 = esnumero()
+
+        print('Ingrese el valor del ángulo: ')
+        ang1 = esnumero()
         
         lado3 = ((lado1**2) + (lado2**2) - (2 * lado1 * lado2 * math.cos(ang1)))*0.5
 
@@ -199,14 +180,15 @@ def main():
             print('Gracias por usar el sistema ')
     
     elif(resp == '4'):
-        lado1 = float(input('Ingrese el valor del lado 1: '))
-        while(lado1 <= 0):
-            print('El valor de un lado debe ser superior a 0')
-            lado1 = float(input('Ingrese el valor del lado 1: '))
-        
-        ang1 = float(input('Ingrese el valor del ángulo 1: '))
 
-        ang2 = float(input('Ingrese el valor del ángulo 2: '))
+        print('Ingrese el valor del lado 1: ')
+        lado1 = esnumero()
+
+        print('Ingrese el valor del ángulo 1: ')
+        ang1 = esnumero()
+
+        print('Ingrese el valor del ángulo 2: ')
+        ang2 = esnumero()
 
         print('El area es: ', round(calcular5(lado1, ang1, ang2),3))
 
